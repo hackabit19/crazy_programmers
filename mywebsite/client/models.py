0039793d3ed28145006ab010889b039b19c7cf68
shikhalakra22 <slakra567@gmail.com>
2019-10-19 03:13:37 +0530

    Removed folder from repository

--- mywebsite/client/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Record(models.Model):
-    sId = models.CharField(max_length=3)
-    sName = models.CharField(max_length=30)
-
-    class Meta:
-        db_table = "student_Record"
